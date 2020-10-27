623dc1a066818fe6b9e6fa6797e2036dc35cee78
David Lehr <david.lehr@daimler.com>
2020-10-27 16:31:44 +0100

    add `disableMarshalingDetection` tag disable current implementation of type reflection, if struct has custom Marshal/Unmarshal implementation

--- definition_builder.go
+++ definition_builder.go
@@ -160,17 +160,19 @@ func (b definitionBuilder) buildProperty(field reflect.StructField, model *spec.
 	}
 	fieldType := field.Type
 
-	// check if type is doing its own marshalling
-	marshalerType := reflect.TypeOf((*json.Marshaler)(nil)).Elem()
-	if fieldType.Implements(marshalerType) {
-		var pType = "string"
-		if prop.Type == nil {
-			prop.Type = []string{pType}
+	if !b.disableMarshalingDetectionOfField(field) {
+		// check if type is doing its own marshalling
+		marshalerType := reflect.TypeOf((*json.Marshaler)(nil)).Elem()
+		if fieldType.Implements(marshalerType) {
+			var pType = "string"
+			if prop.Type == nil {
+				prop.Type = []string{pType}
+			}
+			if prop.Format == "" {
+				prop.Format = b.jsonSchemaFormat(keyFrom(fieldType, b.Config), fieldType.Kind())
+			}
+			return jsonName, modelDescription, prop
 		}
-		if prop.Format == "" {
-			prop.Format = b.jsonSchemaFormat(keyFrom(fieldType, b.Config), fieldType.Kind())
-		}
-		return jsonName, modelDescription, prop
 	}
 
 	// check if annotation says it is a string
@@ -486,6 +488,10 @@ func (b definitionBuilder) jsonNameOfField(field reflect.StructField) string {
 		}
 	}
 	return field.Name
+}
+
+func (b definitionBuilder) disableMarshalingDetectionOfField(field reflect.StructField) bool {
+	return field.Tag.Get("disableMarshalingDetection") == "true"
 }
 
 // see also http://json-schema.org/latest/json-schema-core.html#anchor8
